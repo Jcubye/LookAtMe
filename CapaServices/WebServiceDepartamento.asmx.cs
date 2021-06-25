817cecb24dcf30f6ab685b649496b4c62116de90
Jona <john.cubillos@duocuc.cl>
2021-06-25 08:20:21 -0400

    error app

---
+++ CapaServices/WebServiceDepartamento.asmx.cs
@@ -0,0 +1,29 @@
+﻿using CapaDTO;
+using CapaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CapaServices
+{
+    /// <summary>
+    /// Summary description for WebServiceDepartamento
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebServiceDepartamento : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public void insertarDepartamentoService(Departamento departamento)
+        {
+            NegocioDepartamento auxNegocioDepartamento = new NegocioDepartamento();
+            auxNegocioDepartamento.insertarDepartamento(departamento);
+        }
+    }
+}
